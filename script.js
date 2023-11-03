85f7d487978cb54c547b36be2737bfa10a911ba7
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-03 23:57:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your JS code here. If required.
+let info = document.getElementById("browser-info");
+info.innerHTML=`you are using ${window.navigator.appName} version ${window.navigator.appVersion}`;
